ae668d3689e0bd421e8632fdddf7fb9bfa89f1f0
GProst <gprostmail@gmail.com>
2017-08-24 00:50:14 +0300

    Create initial dark theme

--- src/theme/constants.js
+++ src/theme/constants.js
@@ -59,6 +59,62 @@ const lightTheme = {
   }
 }
 
+const darkTheme = {
+  palette: {
+    'primary': {
+      '50': '#e3f2fd',
+      '100': '#bbdefb',
+      '200': '#90caf9',
+      '300': '#64b5f6',
+      '400': '#42a5f5',
+      '500': '#2196f3',
+      '600': '#1e88e5',
+      '700': '#1976d2',
+      '800': '#1565c0',
+      '900': '#0d47a1',
+      'A100': '#82b1ff',
+      'A200': '#448aff',
+      'A400': '#2979ff',
+      'A700': '#2962ff',
+      'contrastDefaultColor': ThemeType.dark
+    },
+    'accent': {
+      '50': '#fce4ec',
+      '100': '#f8bbd0',
+      '200': '#f48fb1',
+      '300': '#f06292',
+      '400': '#ec407a',
+      '500': '#e91e63',
+      '600': '#d81b60',
+      '700': '#c2185b',
+      '800': '#ad1457',
+      '900': '#880e4f',
+      'A100': '#ff80ab',
+      'A200': '#ff4081',
+      'A400': '#f50057',
+      'A700': '#c51162',
+      'contrastDefaultColor': ThemeType.dark
+    },
+    'error': {
+      '50': '#ffebee',
+      '100': '#ffcdd2',
+      '200': '#ef9a9a',
+      '300': '#e57373',
+      '400': '#ef5350',
+      '500': '#f44336',
+      '600': '#e53935',
+      '700': '#d32f2f',
+      '800': '#c62828',
+      '900': '#b71c1c',
+      'A100': '#ff8a80',
+      'A200': '#ff5252',
+      'A400': '#ff1744',
+      'A700': '#d50000',
+      'contrastDefaultColor': ThemeType.dark
+    }
+  }
+}
+
 const breakpoints = { // TODO
   lgStart: 1366, // large
   mdStart: 1024 // middle
@@ -72,7 +128,7 @@ const mediaQuery = {
   gtMd: `(min-width: ${breakpoints.mdStart}px)`
 }
 
-const themes = new Map([[ThemeType.light, lightTheme]])
+const themes = new Map([[ThemeType.light, lightTheme], [ThemeType.dark, darkTheme]])
 
 export {
   ThemeType,
