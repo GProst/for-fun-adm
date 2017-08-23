8c2a8789ba965d980461c3ee1afd84fadf63ca04
GProst <gprostmail@gmail.com>
2017-08-24 00:05:39 +0300

    Create ThemeProvider

---
+++ src/theme/ThemeProvider.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {ThemeProvider as CSThemeProvider} from 'styled-components'
+import {connect} from 'react-redux'
+import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles'
+import createPalette from 'material-ui/styles/palette'
+
+import {ThemeType, themes} from './constants'
+
+const connector = connect(state => ({
+  theme: state.theme
+}))
+
+class ThemeProvider extends React.Component {
+  static propTypes = {
+    children: PropTypes.any.isRequired,
+    theme: PropTypes.oneOf(Object.values(ThemeType)).isRequired
+  }
+
+  state = {}
+
+  setTheme (themeType) {
+    const theme = themes.get(themeType)
+
+    const muiTheme = createMuiTheme({
+      palette: createPalette({
+        ...theme.palette,
+        type: themeType
+      })
+    })
+
+    console.log('muiTheme', muiTheme)
+
+    this.setState({
+      theme: muiTheme
+    })
+  }
+
+  componentWillMount () {
+    this.setTheme(this.props.theme)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props.theme !== nextProps.theme) {
+      this.setTheme(nextProps.theme)
+    }
+  }
+
+  render () {
+    const {theme} = this.state
+
+    return (
+      <CSThemeProvider theme={theme}>
+        <MuiThemeProvider theme={theme}>
+          {this.props.children}
+        </MuiThemeProvider>
+      </CSThemeProvider>
+    )
+  }
+}
+
+export default connector(ThemeProvider)
