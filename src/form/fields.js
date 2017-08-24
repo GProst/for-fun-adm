4613dfac21473683ebd0d33ca99a385c120edd9f
GProst <gprostmail@gmail.com>
2017-08-24 20:20:58 +0300

    Create FieldShape constant

--- src/form/fields.js
+++ src/form/fields.js
@@ -1,5 +1,15 @@
+import PropTypes from 'prop-types'
+
 export const FieldTypes = {
   email: 0,
   password: 1,
   other: 2
 }
+
+export const FieldShape = PropTypes.shape({
+  value: PropTypes.string,
+  type: PropTypes.oneOf(Object.values(FieldTypes)).isRequired,
+  required: PropTypes.bool,
+  error: PropTypes.string,
+  id: PropTypes.string
+})
